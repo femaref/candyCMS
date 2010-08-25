cae42da8f19e8995964cab80f5b0540bff86035c
Marco Raddatz <github@marcoraddatz.com>
2010-08-25 14:08:06 +0200

    newsletter pdo

--- app/controllers/Newsletter.controller.php
+++ app/controllers/Newsletter.controller.php
@@ -18,20 +18,24 @@ class Newsletter extends Main {
   public final function handleNewsletter() {
     $sMsg = '';
 
-    if( isset($this->_aRequest['email']) &&  ( Helper::checkEmailAddress($this->_aRequest['email']) == false ) )
+    if (isset($this->_aRequest['email']) && ( Helper::checkEmailAddress($this->_aRequest['email']) == false ))
       $sMsg .= Helper::errorMessage(LANG_ERROR_WRONG_EMAIL_FORMAT);
     else {
-      $sQuery = $this->_oModel->handleNewsletter();
-      if($sQuery == 'DESTROY')
-        $sMsg .= Helper::successMessage(LANG_SUCCESS_DESTROY);
-      elseif($sQuery == 'INSERT') {
-        $sMsg .= Helper::successMessage(LANG_SUCCESS_CREATE);
+      if(isset($this->_aRequest['email']) && !empty ($this->_aRequest['email'])) {
+        $sQuery = Model_Newsletter::handleNewsletter(Helper::formatInput($this->_aRequest['email']));
 
-        Mail::send(	Helper::formatInput($this->_aRequest['email'], false),
-                LANG_NEWSLETTER_CREATE_SUCCESS_SUBJECT,
-                LANG_NEWSLETTER_CREATE_SUCCESS_MESSAGE,
-                WEBSITE_MAIL_NOREPLY);
+        if ($sQuery == 'DESTROY')
+          $sMsg .= Helper::successMessage(LANG_SUCCESS_DESTROY);
+        elseif ($sQuery == 'INSERT') {
+          $sMsg .= Helper::successMessage(LANG_SUCCESS_CREATE);
 
+          Mail::send(Helper::formatInput($this->_aRequest['email'], false),
+                          LANG_NEWSLETTER_CREATE_SUCCESS_SUBJECT,
+                          LANG_NEWSLETTER_CREATE_SUCCESS_MESSAGE,
+                          WEBSITE_MAIL_NOREPLY);
+        }
+        else
+          return Helper::errorMessage(LANG_ERROR_DB_QUERY);
       }
     }
 
@@ -43,14 +47,14 @@ class Newsletter extends Main {
     $oSmarty->assign('lang_description', LANG_NEWSLETTER_CREATE_DESTROY_DESCRIPTION);
 
     $oSmarty->template_dir = Helper::getTemplateDir('newsletter/newsletter');
-    return $sMsg.$oSmarty->fetch('newsletter/newsletter.tpl');
+    return $sMsg . $oSmarty->fetch('newsletter/newsletter.tpl');
   }
 
   public function create() {
-    if( USER_RIGHT < 3 )
+    if (USER_RIGHT < 3)
       return Helper::errorMessage(LANG_ERROR_GLOBAL_NO_PERMISSION);
     else {
-      if( isset($this->_aRequest['send_newsletter']) )
+      if (isset($this->_aRequest['send_newsletter']))
         return $this->_newsletterMail();
       else
         return $this->_showCreateNewsletterTemplate();
@@ -59,11 +63,11 @@ class Newsletter extends Main {
 
   private function _showCreateNewsletterTemplate() {
     $sSubject = isset($this->_aRequest['subject']) ?
-            (string)$this->_aRequest['subject']:
+            (string) $this->_aRequest['subject'] :
             '';
 
     $sContent = isset($this->_aRequest['content']) ?
-            (string)$this->_aRequest['content']:
+            (string) $this->_aRequest['content'] :
             '';
 
     $oSmarty = new Smarty();
@@ -98,47 +102,42 @@ class Newsletter extends Main {
       return $sReturn;
     }
     else {
-      # Deliver Newsletter to Users
-      $oGetUser = new Query("	SELECT
-																name, email
-															FROM
-																user
-															WHERE
-																newsletter_default = '1'" );
+      # Deliver Newsletter to users
+      $aResult = Model_Newsletter::getNewsletterRecipients('user');
 
-      while($aRow = $oGetUser->fetch()) {
+      foreach ($aResult as $aRow) {
         $sReceiversName = $aRow['name'];
         $sReceiversMail = $aRow['email'];
 
-        $sMailSubject	= Helper::formatInput($this->_aRequest['subject']);
-        $sMailContent	= Helper::formatInput
-                (	str_replace('%u', $sReceiversName, $this->_aRequest['content']),
-                false
+        $sMailSubject = Helper::formatInput($this->_aRequest['subject']);
+        $sMailContent = Helper::formatInput
+                        (str_replace('%u', $sReceiversName, $this->_aRequest['content']),
+                        false
         );
 
-        Mail::send(	$sReceiversMail, $sMailSubject, $sMailContent);
+        $bStatusUser = Mail::send($sReceiversMail, $sMailSubject, $sMailContent);
       }
 
       # Deliver Newsletter to newsletter-subscripers
-      $oGetUser = new Query("	SELECT
-																email
-															FROM
-																newsletter" );
+      $aResult = Model_Newsletter::getNewsletterRecipients('newsletter');
 
-      while($aRow = $oGetUser->fetch()) {
+      foreach ($aResult as $aRow) {
         $sReceiversName = LANG_NEWSLETTER_DEFAULT_ADDRESS;
         $sReceiversMail = $aRow['email'];
 
-        $sMailSubject	= Helper::formatInput($this->_aRequest['subject']);
-        $sMailContent	= Helper::formatInput
-                (	str_replace('%u', $sReceiversName, $this->_aRequest['content']),
-                true
+        $sMailSubject = Helper::formatInput($this->_aRequest['subject']);
+        $sMailContent = Helper::formatInput
+                        (str_replace('%u', $sReceiversName, $this->_aRequest['content']),
+                        true
         );
 
-        Mail::send(	$sReceiversMail, $sMailSubject, $sMailContent );
+        $bStatusNewsletter = Mail::send($sReceiversMail, $sMailSubject, $sMailContent);
       }
 
-      return Helper::successMessage( LANG_SUCCESS_MAIL_SENT );
+      if($bStatusNewsletter == true && $bStatusUser == true)
+        return Helper::successMessage( LANG_SUCCESS_MAIL_SENT );
+      else
+        return Helper::errorMessage(LANG_ERROR_MAIL_FAILED_SUBJECT);
     }
   }
 }
--- app/models/Newsletter.model.php
+++ app/models/Newsletter.model.php
@@ -8,39 +8,116 @@
 */
 
 class Model_Newsletter extends Model_Main {
-  private function _setData() {
-    if( isset($this->_aRequest['email']) && !empty($this->_aRequest['email']) ) {
-      $oGetData = new Query("	SELECT
-																email
-															FROM
-																newsletter
-															WHERE
-																email ='"	.Helper::formatInput($this->_aRequest['email']).	"'
-															LIMIT
-																1");
+  public static function handleNewsletter($sEmail) {
+    try {
+      $oDb = new PDO('mysql:host=' . SQL_HOST . ';dbname=' . SQL_DB, SQL_USER, SQL_PASSWORD, array(
+                  PDO::ATTR_PERSISTENT => true
+              ));
+      $oDb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-      if($oGetData->numRows() == true) {
-        $oQuery = new Query("	DELETE FROM
-                                newsletter
-                              WHERE
-                                email = '"	.Helper::formatInput($this->_aRequest['email']).	"'
-                              LIMIT 1");
-        return 'DESTROY';
-      }
-      elseif($oGetData->numRows() == false) {
-        $oQuery = new Query("	INSERT INTO
-                                newsletter(email)
-                              VALUES(
-                                '"	.Helper::formatInput($this->_aRequest['email']).	"')");
+      $oQuery = $oDb->prepare("	SELECT
+                                  email
+                                FROM
+                                  newsletter
+                                WHERE
+                                  email = :email
+                                LIMIT
+                                  1");
 
-        return 'INSERT';
+      $oQuery->bindParam('email', $sEmail);
+      $oQuery->execute();
+
+      $aResult = $oQuery->fetch(PDO::FETCH_ASSOC);
+    }
+    catch (AdvancedException $e) {
+      $oDb->rollBack();
+      $e->getMessage();
+    }
+
+    if(isset($aResult) && !empty($aResult['email'])) {
+      try {
+        $oQuery = $oDb->prepare("	DELETE FROM
+                                    newsletter
+                                  WHERE
+                                    email = :email");
+
+        $oQuery->bindParam('email', $sEmail);
+        $bResult = $oQuery->execute();
+
+        $oDb = null;
+
+        if ($bResult == true)
+          return 'DESTROY';# Needed for status message
+        else
+          return false;
       }
-      else
-        return false;
+      catch (AdvancedException $e) {
+        $oDb->rollBack();
+        $e->getMessage();
+      }
+    }
+    else {
+      try {
+        $oQuery = $oDb->prepare(" INSERT INTO
+                                    newsletter(email)
+                                  VALUES
+                                    ( :email )");
+
+        $oQuery->bindParam('email', $sEmail);
+        $bResult = $oQuery->execute();
+
+        $oDb = null;
+
+        if ($bResult == true)
+          return 'INSERT';# Needed for status message
+        else
+          return false;
+      }
+      catch (AdvancedException $e) {
+        $oDb->rollBack();
+        $e->getMessage();
+      }
     }
   }
 
-  public function handleNewsletter() {
-    return $this->_setData();
+  public static function getNewsletterRecipients($sMySqlTable = 'newsletter') {
+    if ($sMySqlTable == 'newsletter') {
+      try {
+        $oDb = new PDO('mysql:host=' . SQL_HOST . ';dbname=' . SQL_DB, SQL_USER, SQL_PASSWORD);
+        $oDb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $oQuery = $oDb->query("	SELECT
+                                  email
+                                FROM
+                                  newsletter");
+
+        return $oQuery->fetchAll(PDO::FETCH_ASSOC);
+      }
+      catch (AdvancedException $e) {
+        $oDb->rollBack();
+        $e->getMessage();
+      }
+    }
+    elseif ($sMySqlTable == 'user') {
+      try {
+        $oDb = new PDO('mysql:host=' . SQL_HOST . ';dbname=' . SQL_DB, SQL_USER, SQL_PASSWORD);
+        $oDb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $oQuery = $oDb->query("	SELECT
+                                  name, email
+                                FROM
+                                  user
+                                WHERE
+                                  newsletter_default = '1'");
+
+        return $oQuery->fetchAll(PDO::FETCH_ASSOC);
+      }
+      catch (AdvancedException $e) {
+        $oDb->rollBack();
+        $e->getMessage();
+      }
+    }
+    else
+      return false;
   }
 }
