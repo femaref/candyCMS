7a5ad5a01950f853c7f8bf687d68e2dd86bbadf1
Marco Raddatz <github@marcoraddatz.com>
2010-09-01 16:09:47 +0200

    migration fix

--- install/migrate.php
+++ install/migrate.php
@@ -14,35 +14,43 @@ $iI = 1;
 $aFiles = array();
 while ($sFile = readdir($oDir)) {
   # Initial fix for older versions
-  try {
-    $oDb = new PDO('mysql:host=' . SQL_HOST . ';dbname=' . SQL_DB, SQL_USER, SQL_PASSWORD);
-    $oDb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $oQuery = $oDb->prepare("SELECT date FROM migration WHERE file = :file");
-    $oQuery->bindParam(':file', $sFile);
-
-    $bReturn = $oQuery->execute();
-
-    if($bReturn == true)
-      $aResult = $oQuery->fetch(PDO::FETCH_ASSOC);
-
-    $oDb = null;
-  } catch (AdvancedException $e) {
-    $oDb->rollBack();
-    $e->getMessage();
+  if(file_exists('migrate/disable_sql.txt')) {
+    $aFiles[0]['name'] = '20100901_add_migrations_to_mysql.sql';
+    $aFiles[0]['query'] = 'Execute this migration and reload page afterwards. If nothing changes,
+      delete "/install/migrate/disable_sql.txt" manually.';
+    @unlink('migrate/disable_sql.txt');
   }
-
-  $bAlreadyMigrated = isset($aResult['date']) && !empty($aResult['date']) ? true : false;
-
-  if (substr($sFile, 0, 1) == '.' || $sFile == '.svn' || $bAlreadyMigrated == true)
-    continue;
   else {
-    $oFo = fopen($sDir. '/' .$sFile, 'r');
-    $sQuery = fread($oFo, filesize($sDir. '/' .$sFile));
+    try {
+      $oDb = new PDO('mysql:host=' . SQL_HOST . ';dbname=' . SQL_DB, SQL_USER, SQL_PASSWORD);
+      $oDb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $aFiles[$iI]['name'] = $sFile;
-    $aFiles[$iI]['query'] = $sQuery;
-    $iI++;
+      $oQuery = $oDb->prepare("SELECT date FROM migration WHERE file = :file");
+      $oQuery->bindParam(':file', $sFile);
+
+      $bReturn = $oQuery->execute();
+
+      if($bReturn == true)
+        $aResult = $oQuery->fetch(PDO::FETCH_ASSOC);
+
+      $oDb = null;
+    } catch (AdvancedException $e) {
+      $oDb->rollBack();
+      $e->getMessage();
+    }
+
+    $bAlreadyMigrated = isset($aResult['date']) && !empty($aResult['date']) ? true : false;
+
+    if (substr($sFile, 0, 1) == '.' || $sFile == '.svn' || $bAlreadyMigrated == true)
+      continue;
+    else {
+      $oFo = fopen($sDir. '/' .$sFile, 'r');
+      $sQuery = fread($oFo, filesize($sDir. '/' .$sFile));
+
+      $aFiles[$iI]['name'] = $sFile;
+      $aFiles[$iI]['query'] = $sQuery;
+      $iI++;
+    }
   }
 }
 
