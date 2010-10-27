e9de20ea5103a635f655345800a84b014827f3ac
Marco Raddatz <github@marcoraddatz.com>
2010-10-27 12:07:13 +0200

    removed old migrations

--- install/migrate/sql/20100905_rename_sql_tables.sql
+++
@@ -1,8 +0,0 @@
-RENAME TABLE `blog` TO `blogs`;
-RENAME TABLE `comment` TO `comments`;
-RENAME TABLE `content` TO `contents`;
-RENAME TABLE `gallery_album` TO `gallery_albums`;
-RENAME TABLE `gallery_file` TO `gallery_files`;
-RENAME TABLE `migration` TO `migrations`;
-RENAME TABLE `newsletter` TO `newsletters`;
-RENAME TABLE `user` TO `users`;
