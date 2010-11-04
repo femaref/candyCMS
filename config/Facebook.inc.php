6bb38a4fce109065a4714626dec645e751c19c5c
Marco Raddatz <github@marcoraddatz.com>
2010-11-04 22:51:29 +0100

    facbook config

---
+++ config/Facebook.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * This software is licensed under GPL <http://www.gnu.org/licenses/gpl.html>.
+ *
+ * @link http://github.com/marcoraddatz/candyCMS
+ * @author Marco Raddatz <http://marcoraddatz.com>
+ */
+
+define('FACEBOOK_APP_ID', '');
+define('FACEBOOK_SECRET', '');
+
+?>
