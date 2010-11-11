1775df430c4f5fe93936a20d68735365606a4a0c
Marco Raddatz <github@marcoraddatz.com>
2010-11-11 20:34:54 +0100

    facebook config

--- config/Facebook.inc.php
+++ config/Facebook.inc.php
@@ -7,7 +7,7 @@
  * @author Marco Raddatz <http://marcoraddatz.com>
  */
 
-define('FACEBOOK_APP_ID', '127081134016102');
-define('FACEBOOK_SECRET', '3e0ba836c70bdecc6f48060fca87d3d1');
+define('FACEBOOK_APP_ID', '');
+define('FACEBOOK_SECRET', '');
 
 ?>
