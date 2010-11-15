b403e525687305e737f1b5f55794b6d144f3bba2
Marco Raddatz <github@marcoraddatz.com>
2010-11-15 15:26:37 +0100

    installation and migration moved to own folder

---
+++ install/install/install.php
@@ -0,0 +1,174 @@
+<?php
+
+/*
+ * This software is licensed under GPL <http://www.gnu.org/licenses/gpl.html>.
+ *
+ * @link http://github.com/marcoraddatz/candyCMS
+ * @author Marco Raddatz <http://marcoraddatz.com>
+ */
+
+switch ($_REQUEST['step']) {
+  default:
+  case '1':
+
+    # Try to create folders (if not avaiable)
+    if (!is_dir('../backup'))
+      @mkdir('../backup', '0777', true);
+
+    if (!is_dir('../cache'))
+      @mkdir('../cache', '0777', true);
+
+    if (!is_dir('../compile'))
+      @mkdir('../compile', '0777', true);
+
+    if (!is_dir('../upload'))
+      @mkdir('../upload', '0777', true);
+
+    if (!is_dir('../upload/gallery'))
+      @mkdir('../upload/gallery', '0777', true);
+
+    if (!is_dir('../upload/media'))
+      @mkdir('../upload/media', '0777', true);
+
+    if (!is_dir('../upload/temp'))
+      @mkdir('../upload/temp', '0777', true);
+
+    if (!is_dir('../upload/temp/media'))
+      @mkdir('../upload/temp/media', '0777', true);
+
+    if (!is_dir('../upload/temp/bbcode'))
+      @mkdir('../upload/temp/bbcode', '0777', true);
+
+    if (!is_dir('../upload/user/32'))
+      @mkdir('../upload/user/32', '0777', true);
+
+    if (!is_dir('../upload/user/64'))
+      @mkdir('../upload/user/64', '0777', true);
+
+    if (!is_dir('../upload/user/100'))
+      @mkdir('../upload/user/100', '0777', true);
+
+    if (!is_dir('../upload/user/' . THUMB_DEFAULT_X))
+      @mkdir('../upload/user/' . THUMB_DEFAULT_X, '0777', true);
+
+    if (!is_dir('../upload/user/popup'))
+      @mkdir('../upload/user/popup', '0777', true);
+
+    if (!is_dir('../upload/user/original'))
+      @mkdir('../upload/user/original/', '0777', true);
+
+    # Visualy print out permissions
+    $sPermissions = "<ul>";
+
+    $sColor = substr(decoct(fileperms("../backup")), 2) == '777' ? 'green' : 'red';
+    $sPermissions .= "<li style='color:" . $sColor . "'>";
+    $sPermissions .= "backup/*";
+    $sPermissions .= "</li>";
+
+    $sColor = substr(decoct(fileperms("../cache")), 2) == '777' ? 'green' : 'red';
+    $sPermissions .= "<li style='color:" . $sColor . "'>";
+    $sPermissions .= "cache/*";
+    $sPermissions .= "</li>";
+
+    $sColor = substr(decoct(fileperms("../compile")), 2) == '777' ? 'green' : 'red';
+    $sPermissions .= "<li style='color:" . $sColor . "'>";
+    $sPermissions .= "compile/*";
+    $sPermissions .= "</li>";
+
+    $sColor = substr(decoct(fileperms("../upload")), 2) == '777' ? 'green' : 'red';
+    $sPermissions .= "<li style='color:" . $sColor . "'>";
+    $sPermissions .= "upload/*";
+    $sPermissions .= "</li>";
+    $sPermissions .= "</ul>";
+
+    $iNextStep = 2;
+    $oSmarty->assign('permissions', $sPermissions);
+    $sHTML = $oSmarty->fetch('showStep1.tpl');
+
+    break;
+  case '2':
+
+    $oSmarty->assign('database', SQL_DB);
+
+    $sHTML = $oSmarty->fetch('showStep2.tpl');
+    $iNextStep = 3;
+
+    break;
+  case '3':
+
+    if (!$_POST)
+      die('No data sent!');
+
+    $sHTML = $oSmarty->fetch('showStep3.tpl');
+    $iNextStep = 4;
+
+    # We create the tables
+    $sUrl = "sql/tables.sql";
+    if (file_exists($sUrl)) {
+      $oFo = fopen($sUrl, 'r');
+      $sData = fread($oFo, filesize($sUrl));
+
+      try {
+        $oDb = new PDO('mysql:host=' . SQL_HOST . ';dbname=' . SQL_DB, SQL_USER, SQL_PASSWORD, array(
+                    PDO::ATTR_PERSISTENT => true
+                ));
+
+        $oQuery = $oDb->query($sData);
+      }
+      catch (AdvancedException $e) {
+        $oDb->rollBack();
+      }
+    }
+
+    $_SESSION['install'] = $_POST;
+    $sPassword = md5(RANDOM_HASH . $_SESSION['install']['password']);
+
+    try {
+      $oQuery = $oDb->prepare(" INSERT INTO
+                                  " . SQL_PREFIX . "users ( id, name, surname, password, email, ip, user_right, date )
+                                VALUES
+                                  ( :id, :name, :surname, :password, :email, :ip, :user_right, :date )");
+
+      $iId = 1;
+      $iUserRight = 4;
+      $oQuery->bindParam('id', $iId, PDO::PARAM_INT);
+      $oQuery->bindParam('name', $_SESSION['install']['name']);
+      $oQuery->bindParam('surname', $_SESSION['install']['surname']);
+      $oQuery->bindParam('password', $sPassword);
+      $oQuery->bindParam('email', $_SESSION['install']['email']);
+      $oQuery->bindParam('ip', $_SERVER['REMOTE_ADDR']);
+      $oQuery->bindParam('user_right', $iUserRight, PDO::PARAM_INT);
+      $oQuery->bindParam('date', time());
+      $bResult = $oQuery->execute();
+
+      $oDb = null;
+    }
+    catch (AdvancedException $e) {
+      $oDb->rollBack();
+    }
+
+    # Create sample content if wanted
+    if (isset($_POST['create_content'])) {
+      $sUrl = "sql/sampledata.sql";
+      if (file_exists($sUrl)) {
+        $oFo = fopen($sUrl, 'r');
+        $sData = fread($oFo, filesize($sUrl));
+        try {
+          $oDb = new PDO('mysql:host=' . SQL_HOST . ';dbname=' . SQL_DB, SQL_USER, SQL_PASSWORD);
+          $oQuery = $oDb->query($sData);
+          $oDb = null;
+        }
+        catch (AdvancedException $e) {
+          $oDb->rollBack();
+        }
+      }
+    }
+
+    break;
+  case '4':
+
+    header('Location:../Session/create');
+
+    break;
+}
+?>
---
+++ install/migrate/migrate.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * This software is licensed under GPL <http://www.gnu.org/licenses/gpl.html>.
+ *
+ * @link http://github.com/marcoraddatz/candyCMS
+ * @author Marco Raddatz <http://marcoraddatz.com>
+ */
+
+require_once '../plugins/Cronjob.class.php';
+
+# Backup database
+if(class_exists('Cronjob') && !file_exists('migrate/sql/20101112_add_id_to_newsletter.sql'))
+  Cronjob::backup(0, '../');
+
+$sDir = 'migrate/sql';
+$oDir = opendir($sDir);
+
+$iI = 1;
+$aFiles = array();
+while ($sFile = readdir($oDir)) {
+  try {
+    $oDb = new PDO('mysql:host=' . SQL_HOST . ';dbname=' . SQL_DB, SQL_USER, SQL_PASSWORD);
+    $oDb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $oQuery = $oDb->prepare("SELECT id FROM " . SQL_PREFIX . "migrations WHERE file = :file");
+    $oQuery->bindParam(':file', $sFile);
+
+    $bReturn = $oQuery->execute();
+
+    if ($bReturn == true)
+      $aResult = $oQuery->fetch(PDO::FETCH_ASSOC);
+
+    $oDb = null;
+  }
+  catch (AdvancedException $e) {
+    $oDb->rollBack();
+    $e->getMessage();
+  }
+
+  $bAlreadyMigrated = isset($aResult['id']) && !empty($aResult['id']) ? true : false;
+
+  if (substr($sFile, 0, 1) == '.' || $sFile == '.svn' || $bAlreadyMigrated == true)
+    continue;
+
+  else {
+    $oFo = fopen($sDir . '/' . $sFile, 'r');
+    $sQuery = fread($oFo, filesize($sDir . '/' . $sFile));
+
+    $aFiles[$iI]['name'] = $sFile;
+    $aFiles[$iI]['query'] = $sQuery;
+    $iI++;
+  }
+
+  unset($bAlreadyMigrated, $aResult);
+}
+
+sort($aFiles);
+
+$oSmarty->assign('files', $aFiles);
+$oSmarty->assign('action', $_SERVER['PHP_SELF']);
+$sHTML = $oSmarty->fetch('showStep1.tpl');
+?>
