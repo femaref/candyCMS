femaref/candyCMS

commit 4482d8fef5f4fea7a720d8cd50b6d143657c474e

fixed mail bug; fixed problems with wrong types

vendor/candyCMS/core/models/Main.model.php
@@ -314,6 +314,10 @@ abstract class Main {
    *
    */
   protected function _formatForUserOutput(&$aData) {
+    # Set up ints first
+    $aData['id']    = (int) $aData['id'];
+    $aData['role']  = (int) $aData['role'];
+
     # Create avatars
     Helper::createAvatarURLs($aData,
             $aData['id'],

vendor/candyCMS/core/models/Sessions.model.php
@@ -55,7 +55,7 @@ class Sessions extends Main {
       if ($bReturn == false)
         self::destroy();
 
-      return $oQuery->fetch(PDO::FETCH_ASSOC);
+      return parent::_formatForUserOutput($oQuery->fetch(PDO::FETCH_ASSOC));
     }
     catch (\PDOException $p) {
       AdvancedException::reportBoth('0072 - ' . $p->getMessage());

app/languages/en.language.yml
@@ -369,7 +369,7 @@ sessions:
     info: Enter your email and we will send you a new password within a few minutes.
     label: Send password
     mail:
-      body: "Hello %s!\r\n\r\nYou can login via following password: <em>%s</em> %%SIGNATURE %%NOREPLY"
+      body: "Hello!\r\n\r\nYou can login via following password: <em>%s</em> %%SIGNATURE %%NOREPLY"
       subject: Your new password
     title: Lost password?
   verification:

vendor/candyCMS/core/controllers/Index.controller.php
@@ -389,11 +389,11 @@ class Index {
     return array(
         'email' => '',
         'facebook_id' => NULL,
-        'id' => 0,
+        'id' => (int) 0,
         'name' => '',
         'surname' => '',
         'password' => '',
-        'role' => 0,
+        'role' => (int) 0,
         'full_name' => ''
     );
   }

vendor/candyCMS/core/models/Users.model.php
@@ -627,7 +627,7 @@ class Users extends Main {
       $oQuery->bindParam('api_token', $sApiToken, PDO::PARAM_STR);
       $oQuery->execute();
 
-      return $oQuery->fetch(PDO::FETCH_ASSOC);
+      return parent::_formatForUserOutput($oQuery->fetch(PDO::FETCH_ASSOC));
     }
     catch (\PDOException $p) {
       AdvancedException::reportBoth('0096 - ' . $p->getMessage());

commit 95bc25adea5edbb2ba65e813dee0f589ad2a3913

fixing refactoring bug introduced in 4482d8fef5f4fea7a720d8cd50b6d143657c474e where non static function was called from static context

vendor/candyCMS/core/models/Main.model.php
@@ -180,10 +180,11 @@ abstract class Main {
   /**
    * Format necessary datetime stamps and add them to $aData
    *
+   * @static
    * @param array $aData array with the timestamp stored in 'date'
    * @return array reference to $aData
    */
-  protected function _formatDates(&$aData) {
+  protected static function _formatDates(&$aData) {
     if (isset($aData['date'])) {
       $aData['date_raw']      = (int) $aData['date'];
       $aData['time']          = Helper::formatTimestamp($aData['date_raw'], 2);
@@ -225,7 +226,7 @@ abstract class Main {
           $aData[$sIdent] = (bool) $aData[$sIdent];
 
     # Format data
-    $this->_formatDates($aData);
+    self::_formatDates($aData);
 
     if (isset($aData['date_raw'])) {
       $iTimestampNow = time();
@@ -291,7 +292,7 @@ abstract class Main {
       );
     }
 
-    $aData['author'] = $this->_formatForUserOutput($aUserData);
+    $aData['author'] = self::_formatForUserOutput($aUserData);
 
     # Encode data for SEO
     $aData['encoded_title'] = isset($aData['title']) ? urlencode($aData['title']) : $aData['author']['encoded_full_name'];
@@ -320,15 +321,17 @@ abstract class Main {
    * Formats / adds all relevant Information for displaying a user.
    *
    * @access protected
+   * @static
    * @param array $aData array of given userdata, required fields are 'email', 'id', 'name', 'surname' and 'use_gravatar'
    * @return array $aData returns reference of $aData
-   * @todo tests
    *
    */
-  protected function _formatForUserOutput(&$aData) {
+  protected static function _formatForUserOutput(&$aData) {
     # Set up ints first
     $aData['id']    = (int) $aData['id'];
     $aData['role']  = (int) isset($aData['role']) ? $aData['role'] : 0;
+
+    self::_formatDates($aData);
 
     # Create avatars
     Helper::createAvatarURLs($aData,
@@ -351,8 +354,6 @@ abstract class Main {
 
     $aData['url_destroy'] = $aData['url_clean'] . '/destroy';
     $aData['url_update']  = $aData['url_clean'] . '/update';
-
-    $this->_formatDates($aData);
 
     return $aData;
   }

vendor/candyCMS/core/models/Sessions.model.php
@@ -55,7 +55,12 @@ class Sessions extends Main {
       if ($bReturn == false)
         self::destroy();
 
-      return parent::_formatForUserOutput($oQuery->fetch(PDO::FETCH_ASSOC));
+      $mData = $oQuery->fetch(PDO::FETCH_ASSOC);
+
+      if ($mData)
+        return parent::_formatForUserOutput($mData);
+      else
+        return $mData;
     }
     catch (\PDOException $p) {
       AdvancedException::reportBoth('0072 - ' . $p->getMessage());

tests/tests/app/models/Main.model.php
@@ -40,7 +40,7 @@ class MyMain extends Main {
   }
 }
 
-class UnitTestOfBlogModel extends CandyUnitTest {
+class UnitTestOfMainModel extends CandyUnitTest {
 
   function setUp() {
     $this->aRequest = array(